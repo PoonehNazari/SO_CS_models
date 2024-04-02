980a78488f3f6ac674e7ea29db8c23337b2ee0c8
Pooneh Nazari <pooneh.nazari@eso.org>
2024-04-02 23:55:35 +0200

    created a script to start writing the inputs

---
+++ physical_model/calc_radiation_fields/write_inputs.py
@@ -0,0 +1,7 @@
+import numpy as np
+import sys
+import read_plot_params as rd_mods
+
+r, theta, r2, theta2,dens_1d, dens_1d_rand, dens_2d, temp_1d, temp_1d_rand, temp_2d, rad_1d, rad_1d_rand,rad_2d, dens_2d_write, temp_2d_write, rad_2d_write = give_grid_dens_temp_rad(M_star_dir)
+myfile = open('/Users/pooneh/Library/Mobile Documents/com~apple~CloudDocs/Academic/ESO/projects/SO_CS_models/inputs/'+filename, 'w')
+myfile.write()
