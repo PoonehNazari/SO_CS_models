d27e4970dc0422a7484d01c38a5b124c49ccdcc7
Pooneh Nazari <pooneh.nazari@eso.org>
2024-04-02 17:42:19 +0200

    decided on the grid points to run RADMC for and am setting up the grid now

--- physical_model/Models/FUV/M_star_0.5/R_disk_50/read_total_flux.py
+++ physical_model/Models/FUV/M_star_0.5/R_disk_50/read_total_flux.py
@@ -6,21 +6,25 @@ from radmc3dPy.natconst import *
 nx =1005
 ny =400
 nz = 1
+p_mas = 1.67e-24 #gr
 #analyze.radmc3dData(grid)
-
+cmap = plt.get_cmap('jet')
+cmap.set_bad(color = 'k')
 plt.figure()
-a    = readData(ddens=True)
+a    = readData(ddens=True,dtemp=True)
 r    = a.grid.x[:]
 theta = a.grid.y[:]
 theta[0]=0.
 theta[-1]=np.pi
 r2,theta2 = np.meshgrid(r/au,theta)
-dens_2d = a.rhodust[:,:,0,0].T
-cmap = plt.get_cmap('tab10')
+dens_2d = a.rhodust[:,:,0,0].T * 100./(1.4* p_mas)
+temp2d = a.dusttemp[:,:,0,0].T
+dens_2d_masked = np.ma.masked_where(dens_2d<1.05e3,dens_2d)
+temp2d_masked = np.ma.masked_where(dens_2d<1.05e3,temp2d)
 c = plt.pcolormesh(r2 * np.sin(theta2), r2 * np.cos(theta2),
-    np.log10(dens_2d), cmap=cmap)
-plt.xlim(0,1000)
-plt.ylim(0,1000)
+    np.log10(temp2d_masked), cmap=cmap)
+plt.xlim(0,80)
+plt.ylim(0,40)
 
 
 data = np.loadtxt('userdef_total_flux.out',skiprows=2)
@@ -31,10 +35,17 @@ if len(data) != hdr[1]:
 data = np.reshape(data, [1,nz, ny, nx])
 data = np.swapaxes(data, 0, 3)
 data = np.swapaxes(data, 1, 2)
-
+data = data[:,:,0,0].T
+data[np.where(data==0.0000000000000000)] = 1e-100
 plt.figure()
+cmap = plt.get_cmap('tab20')
+cmap.set_bad(color = 'k')
+data_masked = np.ma.masked_where(dens_2d<1.05e3,data)
+outflow_elements = np.where(dens_2d<1.05e3)
+data_elemnts = np.where(data==0.0000000000000000)
+print('lalalalallalalallalla',data_elemnts)
 c = plt.pcolormesh(r2 * np.sin(theta2), r2 * np.cos(theta2),
-    np.log10(data[:,:,0,0].T), cmap=cmap, vmax = 6,vmin=-4)
+    np.log10(data_masked), cmap=cmap, vmax = 6,vmin=-4)
 plt.colorbar()
 plt.xlim(0,1000)
 plt.ylim(0,1000)
---
+++ physical_model/Models/FUV/setup_model.py
@@ -0,0 +1,12 @@
+import radmc3dPy 
+import os 
+radmc3dPy.analyze.writeDefaultParfile('disk_env_model_Pooneh_final') 
+radmc3dPy.setup.problemSetupDust('disk_env_model_Pooneh_final',nphot='1000000',nx='[200,100]',ny='200',xbound='[0.4*au,0.5*au,200.0*au]',r_fullenv='200.0*au',mgas='0.7*ms',r_in='0.4*au',prho='-1.5', mstar='0.5*ms', rstar='4.668949248475817*rs',tstar='4000', binary=False,Rc='50.0*au', rdisk='50.0*au', mdisk='0.01*ms',scattering_mode_max='1', Tau='3.0',wbound='[0.0912, 7., 25., 1e4]', nw='[20,100,30]',epsilon = '0.2') 
+os.system('cp -v /Users/pooneh/Library/Mobile\ Documents/com~apple~CloudDocs/Academic/ESO/projects/SO_CS_models/physical_model/Models/FUV/datafiles/dustkappa_silicate.inp .')
+os.system('cp -v /Users/pooneh/Library/Mobile\ Documents/com~apple~CloudDocs/Academic/ESO/projects/SO_CS_models/physical_model/Models/FUV/datafiles/mcmono_wavelength_micron.inp .')
+os.system('radmc3d mctherm')
+os.system('cp -v /Users/pooneh/Library/Mobile\ Documents/com~apple~CloudDocs/Academic/ESO/projects/SO_CS_models/physical_model/Models/FUV/datafiles/main.f90 .')
+os.system('cp -v /Users/pooneh/Library/Mobile\ Documents/com~apple~CloudDocs/Academic/ESO/projects/SO_CS_models/physical_model/Models/FUV/datafiles/userdef_module.f90 .')
+os.system('cp -v /Users/pooneh/Library/Mobile\ Documents/com~apple~CloudDocs/Academic/ESO/projects/SO_CS_models/physical_model/Models/FUV/datafiles/Makefile .')
+os.system('make')
+os.system('./radmc3d mcmono nphot_mono 1000000')
